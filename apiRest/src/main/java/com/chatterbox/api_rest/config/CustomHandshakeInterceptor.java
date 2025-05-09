471506baac768c1c9d6714398d27b77513ca1d4e
luquii12 <lucassanchezsolera@gmail.com>
2025-05-09 18:07:21 +0200

    Acabar chat básico

--- apiRest/src/main/java/com/chatterbox/api_rest/config/CustomHandshakeInterceptor.java
+++ apiRest/src/main/java/com/chatterbox/api_rest/config/CustomHandshakeInterceptor.java
@@ -1,19 +1,18 @@
 package com.chatterbox.api_rest.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 public class CustomHandshakeInterceptor implements HandshakeInterceptor {
 
     @Override
     public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
-// Solucionar error al recuperar los datos
         if (request instanceof ServletServerHttpRequest servletRequest) {
             HttpServletRequest httpRequest = (HttpServletRequest) servletRequest.getServletRequest();
             String token = httpRequest.getParameter("token");
