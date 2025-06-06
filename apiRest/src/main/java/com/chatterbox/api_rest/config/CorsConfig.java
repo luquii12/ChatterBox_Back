f47a4d189cec542d234780df7c05d9ba75ea6a36
luquii12 <lucassanchezsolera@gmail.com>
2025-06-06 03:29:50 +0200

    Revertir los cambios

--- apiRest/src/main/java/com/chatterbox/api_rest/config/CorsConfig.java
+++ apiRest/src/main/java/com/chatterbox/api_rest/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://localhost:5173", "https://chatterboxback-production.up.railway.app")
+                        .allowedOrigins("https://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
